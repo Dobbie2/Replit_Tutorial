242d217f04eeaf33d1bce894ed0ccf072aed19a4
Dobbie2 <martin_clayton@icloud.com>
2023-10-16 20:25:37 +0100

    Final

--- day_18.py
+++ day_18.py
@@ -1,15 +1,24 @@
-number = (79)
+#number = (79)
 
-guess = int(input("What is your guess?: "))
+import random
 
+number = (random.randint(0,100))
+count = 1
 while True:
+    print()
     guess = int(input("What is your guess?: "))
-    if guess == number:
+    if guess < 0:
+        print("Fail!! your guess of",guess, "is not between 0 and",number)
+        exit()
+    elif guess == number:
         break
     elif guess > number:
+        count += 1
         print("Too high")
     elif guess < number:
+        count += 1
         print("Too low")
-    #else:
-        #break
-print ("end")
+print()
+print ("Spot on!")
+print("You completed the gaame with",count,"attempts")
+print()
