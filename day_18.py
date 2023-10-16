b3d95c520f77feca2ed15a043ca7587aafd95d21
Dobbie2 <martin_clayton@icloud.com>
2023-10-16 19:52:37 +0100

    fv2

--- day_18.py
+++ day_18.py
@@ -1,1 +1,15 @@
-print("delete")
+number = (79)
+
+guess = int(input("What is your guess?: "))
+
+while True:
+    guess = int(input("What is your guess?: "))
+    if guess == number:
+        break
+    elif guess > number:
+        print("Too high")
+    elif guess < number:
+        print("Too low")
+    #else:
+        #break
+print ("end")
