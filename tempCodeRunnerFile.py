285e4e4d7508799bda42ac6484bec7eac48fe926
Dobbie2 <martin_clayton@icloud.com>
2023-10-17 17:54:50 +0100

    day 21 complete

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,19 @@
-s = int(input("Start at: "))
-e = int(input("End before: "))
-i = int(input("Increment betwee values: "))
-for i in range(s,e,i):
-  print(i)
+print("Math Game\n")
+a = 0
+b = ""
+while b != "yes":
+  multiples = int(input("\nName your multiples: "))
+  for i in range (1,11):
+    answer = int(input(f'{multiples} x {i} = '))
+    if answer == (i*multiples):
+      print("Great work!\n")
+      a += 1
+    else:
+      print("The answer is",(i*multiples),"\n")
+  if a == 10:
+    print("Amazing! Perfect score! 10 out of 10 \n")
+  else:
+    print(f"Great job!, you get {a} out of 10 \n")
+  b = input("Would you like to exit? ")
+  a = 0
+exit()
