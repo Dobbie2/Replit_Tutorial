e7d034dab68b5819b2e2dbfce4d522beda3c73a2
Dobbie2 <martin_clayton@icloud.com>
2023-10-18 13:26:39 +0100

    day 23 Complete

--- day_23.py
+++ day_23.py
@@ -1,25 +1,35 @@
+def MyName():
+    print("My Name is David")
+    
+MyName()
+
 def rollDice():
   import random
   dice = random.randint(1, 6)
   print("You rolled", dice)
-
-for i in range(10):
+for i in range(100):
     rollDice()
+
+
+
+
+
+
 
 print("Replit Login System")
 print()
 
 def login():
-    userName = input("What is your user Name > ")
-    password = input("What is your password > ")
-    print()
-    print (userName,password)
-    print()
-    if userName != ("david") or password != ("baldies4life"):
-        print("\33[31mWhoops! I don't recognize that username or password. Try again!\33[97m")
+    while True:
+        userName = input("What is your user Name > ")
+        password = input("What is your password > ")
         print()
-    else:
-        print("\33[92mWelcome to Replit!")
-        print()
+        if userName != ("david") or password != ("baldies4life"):
+          print("\33[31mWhoops! I don't recognize that username or password. Try again!\33[97m")
+          print()
+        else:
+          print("\33[92mWelcome to Replit!")
+          print()
+          break
 
 login()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,16 +2,16 @@ print("Replit Login System")
 print()
 
 def login():
-    userName = input("What is your user Name > ")
-    password = input("What is your password > ")
-    print()
-    print (userName,password)
-    print()
-    if userName != ("david") or password != ("baldies4life"):
-        print("\33[31mWhoops! I don't recognize that username or password. Try again!\33[97m")
+    while True:
+        userName = input("What is your user Name > ")
+        password = input("What is your password > ")
         print()
-    else:
-        print("\33[92mWelcome to Replit!")
-        print()
+        if userName != ("david") or password != ("baldies4life"):
+          print("\33[31mWhoops! I don't recognize that username or password. Try again!\33[97m")
+          print()
+        else:
+          print("\33[92mWelcome to Replit!")
+          print()
+          break
 
 login()
