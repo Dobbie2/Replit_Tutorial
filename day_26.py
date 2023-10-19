f42a964123f2f7ee6ad4f933261cc908e56dad49
Dobbie2 <martin_clayton@icloud.com>
2023-10-19 18:59:58 +0100

    Start of day 26

---
+++ day_26.py
@@ -0,0 +1,33 @@
+import os
+for i in range(1,1000):
+  print(i)
+  os.system("clear")
+
+import os, time
+print("Welcome")
+print("to")
+print("Replit")
+time.sleep(2)
+
+os.system("clear")
+
+username = input("Username: ")
+
+from replit import audio
+import os, time
+
+def play():
+  source = audio.play_file('audio.wav')
+  source.paused = False # unpause the playback
+  while True:
+    # Start taking user input and doing something with it
+    input()
+
+while True:
+  # clear the screen 
+
+  # Show the menu
+
+  # take user's input
+
+  # check whether you should call the play() subroutine depending on user's input
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,25 +1,9 @@
+import os, time
+print("Welcome")
+print("to")
+print("Replit")
+time.sleep(2)
 
+os.system("clear")
 
-def dice(number6,number8):
-    import random
-    score = int(random.randint(1,number6)) * int(random.randint(1,number8))
-    return score
-    
-
-print("\033[94m🛡  Character Stats Generator ⚔️exit\033[0m")
-print()
-print("\33[33mType exit to leave the generator\33[97m")
-print()
-while True:
-  
-  finalScore = dice(6,8)
-  name =input("Name your warrior> ")
-  if name == "exit":
-     break
-  elif finalScore >= 24:
-    print ("\33[31mTheir health is",finalScore,"hp\33[97m")
-    print()
-     
-  else:
-    print ("\33[32mTheir health is",finalScore,"hp\33[97m")
-    print()
+username = input("Username: ")
